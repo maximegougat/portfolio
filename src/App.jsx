b0045fba11bf47216fb778fb74be018009562321
maximegougat <contact@maximegougat.com>
2026-01-26 17:06:30 +0100

    Home import from App.jsx corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Home } from "./pages/accueil.jsx"
+import { Home } from "./pages/accueil"
 import { NotFound } from "./pages/NotFound"
 import { Toaster } from "react-hot-toast"
 import LegalNoticePage from "./pages/mentions-legales"
