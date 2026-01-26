390f006865780e571d2b1b81836e6a41b2393310
maximegougat <contact@maximegougat.com>
2026-01-26 16:41:31 +0100

    SpeedInsights import from App.jsx corrected

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Home } from "./pages/accueil.jsx"
 import { NotFound } from "./pages/NotFound"
 import { Toaster } from "react-hot-toast"
 import LegalNoticePage from "./pages/mentions-legales"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
 
   return (
