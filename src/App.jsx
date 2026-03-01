c50734929a382dc619aa47ce07c9814bf8c2594b
maximegougat <contact@maximegougat.com>
2026-03-01 02:11:52 +0100

    responsive website

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import { Home } from "./pages/accueil"
-import { NotFound } from "./pages/NotFound"
 import { Toaster } from "react-hot-toast"
 import LegalNoticePage from "./pages/mentions-legales"
 function App() {
